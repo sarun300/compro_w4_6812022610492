cbf91b7bb023bf4416762ab91dfbf4bfb2af7347
sarun300 <s6812022610492@email.ac.th>
2025-07-14 22:14:55 +0700

    work

---
+++ coding01.c
@@ -0,0 +1,7 @@
+#include <stdio.h>   // Keyword + Header file สำหรับฟังก์ชัน printf
+
+int main() {         // int = Keyword, main = Identifier, () = Operator
+    printf("Hello,\n");     // printf = Identifier, "Hello,\n" = Constant
+    printf("C Tokens!");    // แสดงข้อความอีกบรรทัด
+    return 0;        // return = Keyword, 0 = Constant
+}                     // {} = Operator ใช้กำหนดขอบเขตของ main
